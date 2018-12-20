c9c38daf033079f1b60c8108902e4f63665732f5
AsterDecember <aster.december@gmail.com>
2018-12-19 21:56:01 -0600

    redux working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Routes from "./Routes";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Routes/>
+        <CssBaseline>
+          <Routes/>
+        </CssBaseline>
       </div>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,47 @@
 import React,{Component} from 'react'
+import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {fetchData} from '../actions/exampleActions';
 
 class Home extends Component{
+    componentDidMount() {
+        this.props.fetchData();
+    }
+    triggerAction = () =>{
+        console.log(this.props)
+    }
     render(){
         return(
             <div>
                 Home
+                <Button variant="contained" onClick={this.triggerAction}  >
+                    GO
+                </Button>
             </div>
         )
     }
 
 }
 
-export default Home
+//Set the main stage to props i need to use on this component
+const mapStateToProps = (state) => {
+    const {
+        exampleData
+    } = state;
+
+    return {
+        exampleData
+    };
+};
+
+//Set the acction is going to be trigerred on this component
+const mapDispatchToProps = (dispatch) => {
+
+    return bindActionCreators({
+        fetchData,
+    }, dispatch);
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
